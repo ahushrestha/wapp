a6d9c3c1a2c5a2bd16fee4ea077556d1ad3e3ff5
Ahuti Shrestha <shrstha2@memphis.edu>
2019-02-24 04:29:26 +0000

    Updated quizzing pages to use the quiz model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def home
-    question_id = MultipleChoiceQuestion.all.pluck(:id).sample
+    @quizzes = Quiz.all
+    # @multiple_choice_question = MultipleChoiceQuestion.find(question_id)
+    # question_id = MultipleChoiceQuestion.all.pluck(:id).sample
     # Ahuti: MultipleChoiceQuestion.all -> array of all records
     # Ahuti: pluck(:id) -> we want just id, not all records
     # Ahuti: sample -> randomly choose one
 
-    @multiple_choice_question = MultipleChoiceQuestion.find(question_id)
+    #@multiple_choice_question = MultipleChoiceQuestion.find(question_id)
     # render 'home.html.erb'
   end
 
